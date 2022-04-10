624dd15def39254ccaa2ea248ea94f652c346607
hope9044 <78479712+hope9044@users.noreply.github.com>
2022-04-10 13:20:26 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let words = [{
+let words = [{ //объявление переменной со словами
         word: 'overload',
         trans: 'перезагрузка'
     },
@@ -61,20 +61,20 @@ let words = [{
 
 ]
 
-function speak(text) {
-    const message = new SpeechSynthesisUtterance();
+function speak(text) { //функция для озвучки слов
+    const message = new SpeechSynthesisUtterance(); //объявление экземпляра объекта
     message.lang = "en-EN";
-    message.text = text;
-    window.speechSynthesis.speak(message);
+    message.text = text; //говорим, что озввучивать
+    window.speechSynthesis.speak(message); //обращение к глобальному объекту браузера
 }
 
 function write() {
-    $('.cards_container').empty()
+    $('.cards_container').empty() //затираем все карточки через метод jQuery
     var synth = window.speechSynthesis;
     var voices = synth.getVoices();
-    for (let item = 0; item < words.length; item++) {
-        console.log(words[item])
-        $('.cards_container').append(`
+    for (let item = 0; item < words.length; item++) { //прохоимся по всем словам циклом
+        console.log(words[item]) //с помощью функции jQ мы вставляем разметку в страницу
+        $('.cards_container').append(` 
             <div class="cards_item">
                 <div class="card_word">${words[item].word}</div>
                 <div class="card_translate">${words[item].trans}</div>
@@ -83,10 +83,8 @@ function write() {
             </div>
         `)
     }
-    $('.cards_item').on('click', function(event) {
-        console.log(event.target.classList)
+    $('.cards_item').on('click', function(event) { //назначаем на клик функцию, которая будет показывать перевод по клику на кнопку
         if (event.target.classList.contains('show')) {
-            console.log('yea')
             jQuery(this).find('.card_translate').addClass('show');
         }
         if (event.target.classList.contains('sound')) {
@@ -95,21 +93,20 @@ function write() {
         }
 
     });
-    localStorage.setItem('words', JSON.stringify(words))
+    localStorage.setItem('words', JSON.stringify(words)) //запись в локальное хранилище
 }
 
 $(function() {
     if (localStorage.getItem('words')) {
-        words = JSON.parse(localStorage.getItem('words'))
+        words = JSON.parse(localStorage.getItem('words')) //если в локальном хранилище есть слова, получаем их оттуда
     }
-    write();
-    $('#addW').on('click', function(event) {
+    write(); //вызов функции отрисовки
+    $('#addW').on('click', function(event) { //при клике на кнопку добавить добавляем слова в массив слов
         words.unshift({
-            word: $('#inputs_word').val(),
-            trans: $('#inputs_trans').val(),
-        })
-        write()
-        console.log(words)
+            word: $('#inputs_word').val(), //получаем значение с инпута в котором пишем само слово
+            trans: $('#inputs_trans').val(), //значение с инпута в котором перевод
+        }) //в начало массива добавляем слово
+        write() //вызов функции отрисовки
     })
 
 });
